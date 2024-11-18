4855b2ecdec7172fd49c031201c45a7282ff667b
Minhoangg <132485081+Minhoangg@users.noreply.github.com>
2024-11-18 23:19:23 +0700

    update

--- routes/client/home.php
+++ routes/client/home.php
@@ -7,5 +7,5 @@ Route::prefix('/')->group(function () {
     Route::get('/', [WellcomeController::class, 'index'])->name('home');
     Route::get('/lobby', [WellcomeController::class, 'home'])->name('home-lobby');
     Route::get('/list-game/{id}', [WellcomeController::class, 'details'])->name('list-game');
-    Route::get('get-score/{id}', [WellcomeController::class, 'getScore']);
+    Route::get('get-score/{id}', [WellcomeController::class, 'getScore'])->name('get-score');
 });
