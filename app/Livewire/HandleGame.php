45d3ac3eb2b3e4c3612a9d28580a0fe78a5f144e
Minhoangg <132485081+Minhoangg@users.noreply.github.com>
2024-11-18 14:23:52 +0700

    len

--- app/Livewire/HandleGame.php
+++ app/Livewire/HandleGame.php
@@ -9,10 +9,13 @@ class HandleGame extends Component
 {
     public $id;
     public $percentage = 0;
+
+    public $isLoading = false; // Trạng thái loading
+
     public $roundText = ''; // Lưu trữ giá trị Round
     public $startTime;
     public $endTime;
-    public $isLoading = false; // Trạng thái loading
+
 
     public $nameGame;
     public $imgGame;
@@ -36,8 +39,7 @@ class HandleGame extends Component
 
     public function generateRandomPercentage()
     {
-        // Bắt đầu quá trình loading
-        $this->isLoading = true;
+
 
         $dataGame = Game::find($this->id);
 
@@ -47,21 +49,27 @@ class HandleGame extends Component
         $minpercent = $dataGame->min_percent;
         $maxpercent = $dataGame->max_percent;
 
-        sleep(3); // Giả lập quá trình xử lý 3 giây
+        sleep(3);
 
         $min = rand($minRatio, $maxRatio);
         $max = rand($minRatio, $maxRatio);
+
 
         // Đảm bảo min luôn nhỏ hơn max
         if ($min > $max) {
             list($min, $max) = [$max, $min]; // Hoán đổi nếu min lớn hơn max
         }
 
+
         // Tạo chuỗi hiển thị cho round
         $this->roundText = "Round: $min - $max";
 
-        // Sinh ra tỉ lệ ngẫu nhiên
+        // Sinh ra tỉ lệ ngẫu nhiên sau 10 giây
         $this->percentage = rand($minpercent, $maxpercent);
+
+        $this->isLoading = false; // Kết thúc loading
+
+
 
         $currentDate = now();
 
@@ -70,6 +78,7 @@ class HandleGame extends Component
 
         // Cộng thêm 30 phút vào thời gian bắt đầu để tính thời gian kết thúc
         $endTime = $currentDate->copy()->addMinutes(35)->format('H:i');
+
 
         // Gán giá trị cho các biến
         $this->startTime = $startTime;
